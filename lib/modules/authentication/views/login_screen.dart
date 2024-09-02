66b47a5a99849f859f54b3074ce3a4dd33d32e9e
ahmedabdelrahmanalghwalbi <https://github.com/ahmedabdelrahmanalghwalbi>
2024-09-02 14:27:58 +0300

    login screen

--- lib/modules/authentication/views/login_screen.dart
+++ lib/modules/authentication/views/login_screen.dart
@@ -55,7 +55,7 @@ class LoginScreenState extends State<LoginScreen>
                           alignment:
                               Alignment((viewModel.animation.value * 2) - 1, 0),
                           child: Image.asset(
-                            AppImages.loginBackground,
+                            AppImages.splashScreenBackground,
                             fit: BoxFit.cover,
                           ),
                         ),
@@ -68,102 +68,106 @@ class LoginScreenState extends State<LoginScreen>
                       key: viewModel.formKey,
                       child: Padding(
                         padding: const EdgeInsets.all(AppSizes.s16),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            // Logo
-                            Image.asset(
-                              AppImages.logo,
-                              height: AppSizes.s75,
-                              width: AppSizes.s75,
-                            ),
-                            gapH32,
-                            // Login Page Headline
-                            AutoSizeText(
-                              AppStrings.loginTo.tr(),
-                              textAlign: TextAlign.center,
-                              style: Theme.of(context).textTheme.labelLarge,
-                            ),
-                            AutoSizeText(
-                              AppStrings.yourAccount.tr(),
-                              textAlign: TextAlign.center,
-                              style: Theme.of(context).textTheme.labelLarge,
-                            ),
-                            gapH32,
-                            // TOGGLE BUTTON TO TOGGLE BETWEEN (PHONE || EMAIL)
-                            Consumer<AuthenticationViewModel>(
-                              builder: (context, viewModel, child) {
-                                return SwitchRow(
-                                  isLoginPageStyle: true,
-                                  value: viewModel.isPhoneLogin,
-                                  onChanged: (newValue) =>
-                                      viewModel.toggleLoginMethod(),
-                                );
-                              },
-                            ),
-                            gapH20,
-                            // EMAIL OR PHONE FIELD
-                            Consumer<AuthenticationViewModel>(
-                              builder: (context, viewModel, child) {
-                                return viewModel.isPhoneLogin
-                                    ? PhoneNumberField(
-                                        controller: viewModel.phoneController,
-                                        countryCodeController:
-                                            viewModel.countryCodeController,
-                                      )
-                                    : TextFormField(
-                                        controller: viewModel.emailController,
-                                        decoration: InputDecoration(
-                                          hintText: AppStrings.yourEmail.tr(),
-                                        ),
-                                        validator: (value) =>
-                                            ValidationService.validateEmail(
-                                                value),
-                                      );
-                              },
-                            ),
-                            gapH12,
-                            // PASSWORD FIELD
-                            TextFormField(
-                              controller: viewModel.passwordController,
-                              decoration: InputDecoration(
-                                hintText: AppStrings.password.tr(),
+                        child: SizedBox(
+                          width: LayoutService.getWidth(context),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              // Logo
+                              Image.asset(
+                                AppImages.logo,
+                                width: AppSizes.s200,
+                                height: AppSizes.s70,
+                                fit: BoxFit.contain,
                               ),
-                              validator: (value) =>
-                                  ValidationService.validatePassword(value),
-                              obscureText: true,
-                            ),
-                            gapH12,
-                            // FORGET PASSSORD BUTTON
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                TextButton(
-                                  onPressed: () async {
-                                    FocusManager.instance.primaryFocus
-                                        ?.unfocus();
-                                    await viewModel.showForgotPasswordModal(
-                                      context: context,
-                                    );
-                                  },
-                                  child: Text(AppStrings.forgetPassword.tr(),
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .headlineMedium),
+                              gapH32,
+                              // Login Page Headline
+                              AutoSizeText(
+                                AppStrings.loginTo.tr(),
+                                textAlign: TextAlign.center,
+                                style: Theme.of(context).textTheme.labelLarge,
+                              ),
+                              AutoSizeText(
+                                AppStrings.yourAccount.tr(),
+                                textAlign: TextAlign.center,
+                                style: Theme.of(context).textTheme.labelLarge,
+                              ),
+                              gapH32,
+                              // TOGGLE BUTTON TO TOGGLE BETWEEN (PHONE || EMAIL)
+                              Consumer<AuthenticationViewModel>(
+                                builder: (context, viewModel, child) {
+                                  return SwitchRow(
+                                    isLoginPageStyle: true,
+                                    value: viewModel.isPhoneLogin,
+                                    onChanged: (newValue) =>
+                                        viewModel.toggleLoginMethod(),
+                                  );
+                                },
+                              ),
+                              gapH20,
+                              // EMAIL OR PHONE FIELD
+                              Consumer<AuthenticationViewModel>(
+                                builder: (context, viewModel, child) {
+                                  return viewModel.isPhoneLogin
+                                      ? PhoneNumberField(
+                                          controller: viewModel.phoneController,
+                                          countryCodeController:
+                                              viewModel.countryCodeController,
+                                        )
+                                      : TextFormField(
+                                          controller: viewModel.emailController,
+                                          decoration: InputDecoration(
+                                            hintText: AppStrings.yourEmail.tr(),
+                                          ),
+                                          validator: (value) =>
+                                              ValidationService.validateEmail(
+                                                  value),
+                                        );
+                                },
+                              ),
+                              gapH12,
+                              // PASSWORD FIELD
+                              TextFormField(
+                                controller: viewModel.passwordController,
+                                decoration: InputDecoration(
+                                  hintText: AppStrings.password.tr(),
                                 ),
-                              ],
-                            ),
-                            gapH16,
-                            // LOGIN BUTTON
-                            CustomElevatedButton(
-                              title: AppStrings.login.tr(),
-                              onPressed: () async {
-                                FocusManager.instance.primaryFocus?.unfocus();
-                                await viewModel.login(context: context);
-                              },
-                              isPrimaryBackground: false,
-                            ),
-                          ],
+                                validator: (value) =>
+                                    ValidationService.validatePassword(value),
+                                obscureText: true,
+                              ),
+                              gapH12,
+                              // FORGET PASSSORD BUTTON
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  TextButton(
+                                    onPressed: () async {
+                                      FocusManager.instance.primaryFocus
+                                          ?.unfocus();
+                                      await viewModel.showForgotPasswordModal(
+                                        context: context,
+                                      );
+                                    },
+                                    child: Text(AppStrings.forgetPassword.tr(),
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .headlineMedium),
+                                  ),
+                                ],
+                              ),
+                              gapH16,
+                              // LOGIN BUTTON
+                              CustomElevatedButton(
+                                title: AppStrings.login.tr(),
+                                onPressed: () async {
+                                  FocusManager.instance.primaryFocus?.unfocus();
+                                  await viewModel.login(context: context);
+                                },
+                                isPrimaryBackground: false,
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),

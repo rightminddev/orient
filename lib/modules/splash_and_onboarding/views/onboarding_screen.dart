17f6470234e77dea4a310e1190fa32fc64b2c255
Rana-Yehya <51721428+Rana-Yehya@users.noreply.github.com>
2024-10-13 09:47:16 +0300

    fix gsm and contact bugs

--- lib/modules/splash_and_onboarding/views/onboarding_screen.dart
+++ lib/modules/splash_and_onboarding/views/onboarding_screen.dart
@@ -17,163 +17,169 @@ class OnBoardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<OnboardingViewModel>(
-        create: (context) => OnboardingViewModel(),
-        child: Scaffold(body:
-            Consumer<OnboardingViewModel>(builder: (context, viewModel, child) {
-          return Stack(
-            children: [
-              PageView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  controller: viewModel.pageController,
-                  onPageChanged: (index) => viewModel.currentIndex = index,
-                  itemCount:
-                      viewModel.getAllOnboardingData(context: context)?.length,
-                  itemBuilder: (context, index) {
-                    final image = viewModel
-                        .getOnboardingDataWithIndex(index, context)
-                        ?.image;
-                    if (image?.startsWith('http') == true ||
-                        image?.startsWith('https') == true) {
-                      // Network image
-                      return CachedNetworkImage(
-                        imageUrl: image!,
-                        fit: BoxFit.cover,
-                        key: ValueKey<String>(image),
-                        placeholder: (context, url) =>
-                            const CircularProgressIndicator(),
-                        errorWidget: (context, url, error) =>
-                            const Icon(Icons.error),
-                      );
-                    } else {
-                      // Asset image
-                      return Image.asset(
-                        image!,
-                        fit: BoxFit.cover,
-                        key: ValueKey<String>(image),
-                      );
-                    }
-                  }),
-              // Logo
-              Positioned(
-                top: MediaQuery.of(context).size.height * 0.3,
-                left: AppSizes.s0,
-                right: AppSizes.s0,
-                child: Container(
-                  padding: const EdgeInsets.all(AppSizes.s20),
-                  width: LayoutService.getWidth(context),
-                  height: AppSizes.s125,
-                  child: Center(
-                    child: Image.asset(
-                      AppImages.logo,
-                      fit: BoxFit.fill,
-                      key: const ValueKey<String>(AppImages.logo),
-                    ),
-                  ),
-                ),
-              ),
-
-              Positioned(
-                bottom: AppSizes.s48,
-                left: AppSizes.s0,
-                right: AppSizes.s0,
-                child: Card(
-                  margin: const EdgeInsets.symmetric(horizontal: AppSizes.s16),
-                  clipBehavior: Clip.antiAlias,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(AppSizes.s25),
-                  ),
+      create: (context) => OnboardingViewModel(),
+      child: Scaffold(
+        body: Consumer<OnboardingViewModel>(
+          builder: (context, viewModel, child) {
+            return Stack(
+              children: [
+                PageView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    controller: viewModel.pageController,
+                    onPageChanged: (index) => viewModel.currentIndex = index,
+                    itemCount: viewModel
+                        .getAllOnboardingData(context: context)
+                        ?.length,
+                    itemBuilder: (context, index) {
+                      final image = viewModel
+                          .getOnboardingDataWithIndex(index, context)
+                          ?.image;
+                      if (image?.startsWith('http') == true ||
+                          image?.startsWith('https') == true) {
+                        // Network image
+                        return CachedNetworkImage(
+                          imageUrl: image!,
+                          fit: BoxFit.cover,
+                          key: ValueKey<String>(image),
+                          placeholder: (context, url) =>
+                              const CircularProgressIndicator(),
+                          errorWidget: (context, url, error) =>
+                              const Icon(Icons.error),
+                        );
+                      } else {
+                        // Asset image
+                        return Image.asset(
+                          image!,
+                          fit: BoxFit.cover,
+                          key: ValueKey<String>(image),
+                        );
+                      }
+                    }),
+                // Logo
+                Positioned(
+                  top: MediaQuery.of(context).size.height * 0.3,
+                  left: AppSizes.s0,
+                  right: AppSizes.s0,
                   child: Container(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: AppSizes.s18, vertical: AppSizes.s20),
-                    height: AppSizes.s300,
-                    child: Column(
-                      children: [
-                        Expanded(
-                          flex: 5,
-                          child: PageView.builder(
-                            physics: const NeverScrollableScrollPhysics(),
-                            controller: viewModel.pageController2,
-                            itemCount: viewModel
-                                .getAllOnboardingData(context: context)
-                                ?.length,
-                            itemBuilder: (context, index) {
-                              return Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  AutoSizeText(
-                                    viewModel
-                                            .getOnboardingDataWithIndex(
-                                                index, context)
-                                            ?.title ??
-                                        '',
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .displayLarge
-                                        ?.copyWith(height: 1.2),
-                                    textAlign: TextAlign.center,
-                                    maxLines: 2,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                  gapH20,
-                                  AutoSizeText(
-                                    viewModel
-                                            .getOnboardingDataWithIndex(
-                                                index, context)
-                                            ?.info ??
-                                        "",
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .displaySmall
-                                        ?.copyWith(height: 1.4),
-                                    textAlign: TextAlign.center,
-                                    maxLines: 4,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                ],
-                              );
-                            },
-                          ),
-                        ),
-                        Expanded(
-                          flex: 2,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              CustomElevatedButton(
-                                  onPressed: () async =>
-                                      viewModel.goNext(context),
-                                  title: AppStrings.next.tr(),
-                                  width: AppSizes.s150,
-                                  isPrimaryBackground: true,
-                                  isFuture: false),
-                              TextButton(
-                                onPressed: () => viewModel.skip(context),
-                                style: ElevatedButton.styleFrom(
-                                  fixedSize:
-                                      const Size(AppSizes.s150, AppSizes.s50),
-                                ),
-                                child: Text(AppStrings.skip.tr(),
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .headlineSmall
-                                        ?.copyWith(
-                                            fontWeight: FontWeight.bold,
-                                            color: Theme.of(context)
-                                                .colorScheme
-                                                .secondary)),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
+                    padding: const EdgeInsets.all(AppSizes.s20),
+                    width: LayoutService.getWidth(context),
+                    height: AppSizes.s125,
+                    child: Center(
+                      child: Image.asset(
+                        AppImages.logo,
+                        fit: BoxFit.fill,
+                        key: const ValueKey<String>(AppImages.logo),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const LanguageDropdownButton()
-            ],
-          );
-        })));
+
+                Positioned(
+                  bottom: AppSizes.s48,
+                  left: AppSizes.s0,
+                  right: AppSizes.s0,
+                  child: Card(
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: AppSizes.s16),
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(AppSizes.s25),
+                    ),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: AppSizes.s18, vertical: AppSizes.s20),
+                      height: AppSizes.s300,
+                      child: Column(
+                        children: [
+                          Expanded(
+                            flex: 5,
+                            child: PageView.builder(
+                              physics: const NeverScrollableScrollPhysics(),
+                              controller: viewModel.pageController2,
+                              itemCount: viewModel
+                                  .getAllOnboardingData(context: context)
+                                  ?.length,
+                              itemBuilder: (context, index) {
+                                return Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    AutoSizeText(
+                                      viewModel
+                                              .getOnboardingDataWithIndex(
+                                                  index, context)
+                                              ?.title ??
+                                          '',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .displayLarge
+                                          ?.copyWith(height: 1.2),
+                                      textAlign: TextAlign.center,
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    gapH20,
+                                    AutoSizeText(
+                                      viewModel
+                                              .getOnboardingDataWithIndex(
+                                                  index, context)
+                                              ?.info ??
+                                          "",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .displaySmall
+                                          ?.copyWith(height: 1.4),
+                                      textAlign: TextAlign.center,
+                                      maxLines: 4,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ],
+                                );
+                              },
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                CustomElevatedButton(
+                                    onPressed: () async =>
+                                        viewModel.goNext(context),
+                                    title: AppStrings.next.tr(),
+                                    width: AppSizes.s150,
+                                    isPrimaryBackground: true,
+                                    isFuture: false),
+                                TextButton(
+                                  onPressed: () => viewModel.skip(context),
+                                  style: ElevatedButton.styleFrom(
+                                    fixedSize:
+                                        const Size(AppSizes.s150, AppSizes.s50),
+                                  ),
+                                  child: Text(AppStrings.skip.tr(),
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .headlineSmall
+                                          ?.copyWith(
+                                              fontWeight: FontWeight.bold,
+                                              color: Theme.of(context)
+                                                  .colorScheme
+                                                  .secondary)),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const LanguageDropdownButton()
+              ],
+            );
+          },
+        ),
+      ),
+    );
   }
 }
